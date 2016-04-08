10859088d10c20ba3a10f9131d1e18b990c5ec37
spencer-langley <mrbigulls@hotmail.com>
2016-04-07 20:08:47 -0500

    Fixed answer check in admin

--- alfariq/ViewModels/CompletedSessionViewModel.cs
+++ alfariq/ViewModels/CompletedSessionViewModel.cs
@@ -102,7 +102,7 @@ namespace alfariq.ViewModels
             Option2 = t.Option2.English;
             Option3 = t.Option3.English;
             PromptedWord = t.Word.Arabic;
-            Correct = t.Word.Id == t.Answer.Id;
+            Correct = (t.Word.English.Equals(t.Answer.English));
         }
     }
 }
